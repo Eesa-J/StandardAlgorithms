52cb20435000e8fbaf02ff70eb2006428f70bc92
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2022-11-05 11:22:50 +0000

    Added function to generate random numbers.

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -7,4 +7,17 @@
 
 import Foundation
 
+func randomFunction(number: Int) {
+    for _ in 1...number {
+        print(Int.random(in: 0...100))
+    }
+}
+
 print("Hello, Eesa!")
+
+let question = "How are you today"
+let message = "\(question)?"
+
+randomFunction(number: 10)
+
+print(message)
